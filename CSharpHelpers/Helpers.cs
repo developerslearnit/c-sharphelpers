5578ad1fd6674189358d8c5129dab4ad499a490e
Adesina <omark@simplexsystem.com>
2021-06-10 12:19:53 +0100

    Initial Commit

---
+++ CSharpHelpers/Helpers.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace CSharpHelpers
+{
+    public static class Helpers
+    {
+        /// <summary>
+        /// Get substring of specified number of characters on the right.
+        /// </summary>
+        public static string Right(this string value, int length)
+        {
+            if (String.IsNullOrEmpty(value)) return string.Empty;
+
+            return value.Length <= length ? value : value.Substring(value.Length - length);
+        }
+
+        /// <summary>
+        /// Get substring of specified number of characters on the left.
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException">Public key not found. Private key not found. Missing password</exception>
+        public static string Left(this string value, int length)
+        {
+            if (String.IsNullOrEmpty(value)) return string.Empty;
+
+            return value.Length <= length ? value : value.Substring(0, length);
+        }
+
+        /// <summary>
+        /// Convert a given value to base64 string
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns>Base64 value</returns>
+        public static string ToBase64(this string value)
+        {
+            if (String.IsNullOrEmpty(value)) return string.Empty;
+
+            byte[] data = ASCIIEncoding.ASCII.GetBytes(value);
+
+            return Convert.ToBase64String(data);
+        }
+
+
+        /// <summary>
+        /// Convert baase64 string to normal string
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns>String</returns>
+
+        public static string FromBase64(this string value)
+        {
+
+            if (String.IsNullOrEmpty(value)) return string.Empty;
+
+            byte[] data = Convert.FromBase64String(value);
+
+            return ASCIIEncoding.ASCII.GetString(data);
+
+        }
+
+        /// <summary>
+        /// Verifies that a string is in valid e-mail format
+        /// </summary>
+        /// <param name="email">Email to verify</param>
+        /// <returns>true if the string is a valid e-mail address and false if it's not</returns>
+        public static bool isValidEmail(this string email)
+        {
+            if (String.IsNullOrEmpty(email))
+                return false;
+            //we use EmailValidator from FluentValidation. So let's keep them sync - https://github.com/JeremySkinner/FluentValidation/blob/master/src/FluentValidation/Validators/EmailValidator.cs
+            string emailRegex = @"^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-||_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+([a-z]+|\d|-|\.{0,1}|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])?([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$";
+
+            email = email.Trim();
+            var result = Regex.IsMatch(email, emailRegex, RegexOptions.IgnoreCase);
+            return result;
+        }
+
+        /// <summary>
+        ///     Converts string to its boolean equivalent
+        /// </summary>
+        /// <param name="value">string to convert</param>
+        /// <returns>boolean equivalent</returns>
+        /// <remarks>
+        ///     <exception cref="ArgumentException">
+        ///         thrown in the event no boolean equivalent found or an empty or whitespace
+        ///         string is passed
+        ///     </exception>
+        /// </remarks>
+        public static bool ToBool(this string value)
+        {
+            if (string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentException("value");
+            }
+            string val = value.ToLower().Trim();
+            switch (val)
+            {
+                case "false":
+                    return false;
+                case "f":
+                    return false;
+                case "true":
+                    return true;
+                case "t":
+                    return true;
+                case "yes":
+                    return true;
+                case "no":
+                    return false;
+                case "y":
+                    return true;
+                case "n":
+                    return false;
+                default:
+                    throw new ArgumentException("Invalid boolean");
+            }
+        }
+
+
+        /// <summary>
+        /// Returns an empty string if null or empty is passed
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns>System.String</returns>
+        public static string EmptyIfNull(this string value)
+        {           
+            return string.IsNullOrWhiteSpace(value) ? "" : value.Trim();
+        }
+
+
+        /// <summary>
+        ///     IsNumeric checks if a string is a valid floating value
+        /// </summary>
+        /// <param name="val"></param>
+        /// <returns>Boolean True if isNumeric else False</returns>
+        /// <remarks></remarks>
+        public static bool IsNumeric(this string val)
+        {
+            // Variable to collect the Return value of the TryParse method.
+
+            // Define variable to collect out parameter of the TryParse method. If the conversion fails, the out parameter is zero.
+            double retNum;
+
+            // The TryParse method converts a string in a specified style and culture-specific format to its double-precision floating point number equivalent.
+            // The TryParse method does not generate an exception if the conversion fails. If the conversion passes, True is returned. If it does not, False is returned.
+            bool isNum = Double.TryParse(val, NumberStyles.Any, NumberFormatInfo.InvariantInfo, out retNum);
+            return isNum;
+        }
+
+        /// <summary>
+        ///     IsInteger Function checks if a string is a valid int32 value
+        /// </summary>
+        /// <param name="val">val</param>
+        /// <returns>Boolean True if isInteger else False</returns>
+        public static bool IsInteger(this string val)
+        {
+            // Variable to collect the Return value of the TryParse method.
+
+            // Define variable to collect out parameter of the TryParse method. If the conversion fails, the out parameter is zero.
+            int retNum;
+
+            // The TryParse method converts a string in a specified style and culture-specific format to its double-precision floating point number equivalent.
+            // The TryParse method does not generate an exception if the conversion fails. If the conversion passes, True is returned. If it does not, False is returned.
+            bool isNum = Int32.TryParse(val, NumberStyles.Any, NumberFormatInfo.InvariantInfo, out retNum);
+            return isNum;
+        }
+
+        /// <summary>
+        ///     Convert a string to its equivalent byte array
+        /// </summary>
+        /// <param name="val">string to convert</param>
+        /// <returns>System.byte array</returns>
+        public static byte[] ToBytes(this string val)
+        {
+            var bytes = new byte[val.Length * sizeof(char)];
+            Buffer.BlockCopy(val.ToCharArray(), 0, bytes, 0, bytes.Length);
+            return bytes;
+        }
+
+
+        /// <summary>
+        ///     Reverse string
+        /// </summary>
+        /// <param name="val">string to reverse</param>
+        /// <returns>System.string</returns>
+        public static string Reverse(this string val)
+        {
+            var chars = new char[val.Length];
+            for (int i = val.Length - 1, j = 0; i >= 0; --i, ++j)
+            {
+                chars[j] = val[i];
+            }
+            val = new String(chars);
+            return val;
+        }
+
+
+    }
+}
